cc98e02ed83cf88a348e706daaa528d70456cdcb
yossyyai <yoshiyukiui.ocean@gmail.com>
2021-03-22 19:52:33 +0900

    Revert "comaモデルの作成"
    This reverts commit 32b9a63bfcbbf70413cf33f70a2bae22fe337114.

--- db/migrate/20210322100628_create_comas.rb
+++
@@ -1,9 +0,0 @@
-class CreateComas < ActiveRecord::Migration[6.0]
-  def change
-    create_table :comas do |t|
-      t.references :user, foreign_key: true
-      t.references :room, foreign_key: true
-      t.timestamps
-    end
-  end
-end
